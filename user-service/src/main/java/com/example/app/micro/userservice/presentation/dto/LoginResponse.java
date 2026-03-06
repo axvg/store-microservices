7cb1b14049d4f1606d209289ea6b3b5ca2ded4b4
Alex Vega <axvg@users.noreply.github.com>
2026-03-06 17:47:20 -0500

    add dto

---
+++ user-service/src/main/java/com/example/app/micro/userservice/presentation/dto/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.example.app.micro.userservice.presentation.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String type;
+    private String email;
+    private List<String> roles;
+}
