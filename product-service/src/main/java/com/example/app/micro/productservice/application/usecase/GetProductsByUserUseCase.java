3448245307819396464148f1c074e115b69bcae1
Alex Vega <axvg@users.noreply.github.com>
2026-03-08 11:35:37 -0500

    feat: update usecase

--- product-service/src/main/java/com/example/app/micro/productservice/application/usecase/GetProductsByUserUseCase.java
+++ product-service/src/main/java/com/example/app/micro/productservice/application/usecase/GetProductsByUserUseCase.java
@@ -4,11 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import com.example.app.micro.productservice.domain.exception.UserNotFoundException;
 import com.example.app.micro.productservice.domain.model.Product;
-import com.example.app.micro.productservice.domain.model.User;
 import com.example.app.micro.productservice.domain.repository.ProductRepository;
-import com.example.app.micro.productservice.infrastructure.client.UserClient;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,18 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GetProductsByUserUseCase {
     private final ProductRepository repo;
-    private final UserClient client;
 
     public List<Product> execute(Long userId) {
-        User user = client.getUserById(userId);
-
-        log.info("Fetching products for user from userdb: {}", user.getName());
-        
-        if (user == null) {
-            log.warn("User with id {} not found in userdb", userId);
-            throw new UserNotFoundException(userId);
-        }
-
         log.debug("Executing GetProductsByUserUseCase with userId: {}", userId);
         return repo.findByCreatedBy(userId);
     }
